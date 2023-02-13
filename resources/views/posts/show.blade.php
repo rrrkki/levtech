e760ac4089bd2d94c2a47f590c22c80ef23315ca
rrkkki <r.k.t.5@icloud.com>
2023-02-13 20:32:57 +0900

    8-5課題

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,13 +13,16 @@
             {{ $post->title }}
          </h1>
         <div class="content">
-              <div class="content_post">
+             <div class="content_post">
              <h3>本文</h3>
                  <p>{{ $post->body }}</p>
-              </div>
-         </div>
-            <div class="footer">
-                <a href="/">戻る</a>
-            </div>
+             </div>
+                <div class="edit">
+                <a href="/posts/{{ $post->id}}/edit">edit</a>
+                </div>
+                   <div class="footer">
+                   <a href="/">戻る</a>
+                   </div>
+        </div>
     </body>
 </html>
